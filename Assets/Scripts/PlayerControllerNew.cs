4937636200334639a5da613125827c4eea3b3022
Karina <karina.popp@nyu.edu>
2022-11-07 18:02:31 -0500

    Touch effects + new player controller file

--- Assets/Scripts/PlayerControllerNew.cs
+++ Assets/Scripts/PlayerControllerNew.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerNew : MonoBehaviour
 {
     SpriteRenderer myRenderer;
     public Color baseColor;
